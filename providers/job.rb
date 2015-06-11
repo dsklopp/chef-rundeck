8c80de79eea6e8040483cb94a552610230fdcd5f
git <dsklopp@gmail.com>
2015-06-10 21:23:38 -0700

    Removing hardcoded IP

--- providers/job.rb
+++ providers/job.rb
@@ -43,7 +43,7 @@ def create_job
 end
 
 def list_job(token, project)
-   uri = URI.parse("http://192.168.17.118:4440")
+   uri = URI.parse("http://#{node['rundeck']['hostname']}:#{node['rundeck']['port']}")
    http = Net::HTTP.new(uri.host, uri.port)
    http.use_ssl = false
    request = Net::HTTP::Get.new("/api/2/project/#{project}/jobs")
